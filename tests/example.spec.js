36b9abe2bdb9102ef4e6def0477c91d92a9a635d
Franklin John Clavecillas <franklin@bizbox.ph>
2025-01-08 07:04:25 +0800

    initial

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.devs/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
